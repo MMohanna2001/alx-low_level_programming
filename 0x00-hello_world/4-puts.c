80a9d34e514917bfe88b529ce725a6e79b197f52
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-14 23:43:52 -0700

    1st

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/*
+ * Using puts Function
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
