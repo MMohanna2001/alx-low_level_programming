850d53ac70f6cb190ce8613c20d54a580ecbb416
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-16 06:27:17 -0700

    2nd

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,8 +11,8 @@ int main(void)
 	char c = '0';
 
 	for (c = '0'; c <= '9'; c++)
-		putcahr(c);
-	for (c = 'a'; c <= f; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
 
