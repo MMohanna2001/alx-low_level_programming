7efcd52c12d663306c2b6260a4863844d9b88368
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-20 19:25:06 -0700

    3rd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Check a number 
+ * @n: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}	
