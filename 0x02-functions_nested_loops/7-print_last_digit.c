8df63689e775bcd528df649e902d45a8ba96b6b7
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-20 19:43:44 -0700

    3rd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Check if a number is greater than 0
+ * @num: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
