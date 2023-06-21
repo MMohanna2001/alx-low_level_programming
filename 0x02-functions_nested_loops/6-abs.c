3bcbea6bf6d034e60a27fcc8aaeaff7da6283a43
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-20 19:36:44 -0700

    3rd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _abs - absolute of a function 
+ * @c: number tyo be computed
+ *
+ * Return: absolute of the input number
+ */
+
+
+int _abs(int c)
+{
+	return (abs(c));
+}
