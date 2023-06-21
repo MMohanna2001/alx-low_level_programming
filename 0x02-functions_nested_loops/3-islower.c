41263968faf54ac83abcb8046f5d2c2d63c9f04d
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-20 18:27:28 -0700

    3rd

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+
+	else
+		return (0);
+
