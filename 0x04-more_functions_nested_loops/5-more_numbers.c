8e6bd45949df6d222d966e30730d1bbb63cda071
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-22 15:42:46 -0700

    4th

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints "Hello"
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if ((i / 10) != 0)
+				_putchar('0' + (i / 10));
+
+			_putchar('0' + (i % 10));
+		}
+
+	}
+
+	_putchar('\n');
+}
