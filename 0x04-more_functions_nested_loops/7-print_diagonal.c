2394908bbbfc49aeef9152cb35dbd1156ec84d13
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-22 16:07:19 -0700

    4th

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Check if a number is greater than 0
+ * @n: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n - 1 ; i++)
+		_putchar(' ');
+
+	_putchar('\\');
+	_putchar('\n');
+}
