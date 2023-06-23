d4c8b770175fad03df413620c656cc4fed3952c9
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-22 19:09:14 -0700

    5th

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - Check if a number is greater than 0
+ * @size: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+void print_triangle(int size)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k < i + 1; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
