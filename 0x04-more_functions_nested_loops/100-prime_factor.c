b82e682c0ebbf3786e12c06f418589f3c6baf42d
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-22 19:37:52 -0700

    5th

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long long num = 612852475143;
+	long long max = 0;
+	int i = 0;
+	int j = 0;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		num = num / 2;
+	}
+
+	while (n % 3 == 0)
+	{
+		max = 3;
+		num = num / 3;
+	}
+
+	for (i = 5; i <= sqrt(num); i += 6)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			num = num / i;
+		}
+		while (n % (i + 2))
+		{
+			max = i + 2;
+			num = num / (i + 2);
+		}
+	}
+
+	printf("%ll", max);
+}
