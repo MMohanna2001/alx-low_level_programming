d39bc0d29299e38c4bb5bf6541aa69f5f5894098
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-22 19:52:14 -0700

    5th

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Check if a number is greater than 0
+ * @n: The number to be checked
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+void print_number(int n)
+{
+	int j = 0;
+
+	while (n != 0)
+	{
+		j = j * 10;
+		j = j + (n % 10);
+		n = n / 10;
+	}
+	while (j != 0)
+	{
+		_putchar('0' + (j % 10));
+		j = j / 10;
+	}
+}
