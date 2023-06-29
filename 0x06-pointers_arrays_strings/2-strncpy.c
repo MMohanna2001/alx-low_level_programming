ddd2d5bbbd77570525066c5478ee56298d8d67b4
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-29 12:06:13 -0700

    7th

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncopy - Connect
+ * @dest: First operand
+ * @src: Second operand
+ * @n: Third
+ *
+ * Return: The sum of the two parameters
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+
+	strncopy(dest, src, n);
+	return (dest);
+}
