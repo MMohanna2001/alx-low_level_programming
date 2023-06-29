4670da5886c88a2832a5882a02e54c8b4667ba48
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-29 09:25:57 -0700

    7th

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Connect
+ * @dest: First operand
+ * @src: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
