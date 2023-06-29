1c26689a6445526b3c7812d224c7bfa1cd4892e8
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-06-29 13:32:39 -0700

    7th

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,23 @@
  * Return: The sum of the two parameters
  */
 
-char *cap_string(char *)
+char *cap_string(char *c)
 {
 
 	int n = 0;
+	char *mainchars = ",;.!?'\"(){} \n\t";
+
+	if (c[n] >= 'a' && c[n] <= 'z')
+		c[n] = c[n] - 32;
+
 
 	while (c[n] != '\0')
 	{
-		if (c[n] >= 'a' && c[n] <= 'z')
-			c[n] = c[n] - 32;
+		if (strchr(mainchars, c[n]) != NULL)
+		{
+			if (c[n + 1] >= 'a' && c[n + 1] <= 'z')
+				c[n + 1] = c[n + 1] - 32;
+		}
 		n++;
 	}
 
