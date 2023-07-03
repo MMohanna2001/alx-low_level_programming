5e972b7ca92ed30405212f95df1ad60e7dc00a3a
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-03 11:58:58 -0700

    8th

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_number - Connect
+ * @n: First operand
+ *
+ * Return: The sum of the two parameters
+ */
+
+void print_number(int n)
+{
+	int num = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n != 0)
+	{
+	 num = num * 10 + (n % 10);
+	 n = n / 10;
+	}
+
+	while (num != 0)
+	{
+		_putchar('0' + (num % 10));
+		num = num / 10;
+	}
+
+	_putchar('\n');
+}
