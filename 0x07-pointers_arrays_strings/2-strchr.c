48e7184f20ca955c38effaeeb57044227f00f53b
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-03 02:49:04 -0700

    8th

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - see code
+ * @s: arg1
+ * @c: arg2
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+
+	return (s);
+}
