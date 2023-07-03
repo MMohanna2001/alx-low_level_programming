6d28823c410932aded2b3599495cda5b0a1499eb
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-03 03:23:23 -0700

    8th

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_diagsums - see code
+ * @a: arg1
+ * @size: arg2
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i][i];
+		sum2 = sum2 + a[i][size - 1 - i];
+	}
+
+	printf("%d, %d\n", sum1, sum2); 
+}
