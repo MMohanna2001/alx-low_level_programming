b239818c6af1b6ba7fc7f7366d4dfda1b05e7c2f
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-03 07:46:46 -0700

    8th

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * set_string - see code
+ * @s: arg1
+ * @to: arg2
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
