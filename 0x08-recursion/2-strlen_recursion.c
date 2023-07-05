5ba2f9d6a1f5c2452397c1d89d5321a4e6913e31
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-05 10:50:00 -0700

    9th

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - see code
+ * @s: arg1
+ *
+ * Return: print
+ */
+
+int _strlen_recursion(char *s);
+{
+	int len = 0;
+
+	if (s[0] != '\0')
+	{
+		len = len + 1;
+		len += _strlen_recursion(s + 1);
+
+	}
+
+	return (len);
+
+}
