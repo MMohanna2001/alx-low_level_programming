0881eedda6f73b04cb2541cfce85ca7fcb6c8e0c
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-05 10:35:56 -0700

    9th

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - see code
+ * @s: arg1
+ *
+ * Return: print
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(s[0]);
+	}
+
+}
