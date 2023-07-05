d9e9f1f5fa6acd92ca7ebace756072535f82f83d
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-05 10:30:59 -0700

    9th

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - see code
+ * @s: arg1
+ *
+ * Return: print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		puts(s[0]);
+		_puts_recursion(s + 1);
+	}
+
+	else 
+		_putchar('\n');
+}
