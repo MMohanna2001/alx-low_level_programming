2faee857535be6325fb77cc2a8bc80f45b79fdf6
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-07 10:15:10 -0700

    10th

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <srdlib>
+/**
+ * main - Code
+ * @argc: arg1
+ * @argv: arg2
+ *
+ * Return: The sum of the two parameters
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1;
+	int n2;
+	int product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
+	product = n1 * n2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
