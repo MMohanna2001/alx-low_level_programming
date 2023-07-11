8c4c22c87fd2d3fd2db6d59778470eb7a5e03f09
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-11 13:43:26 -0700

    11th

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,44 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * str_concat - Code
- * @s1: arg1
- * @s2: arg2
+ * _strdup - Code
+ * @str: arg1
  * Return: re
  *
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	char *str;
-	int count1 = 0;
-	int count2 = 0;
-	int i = 0;
-	int j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	count1 = strlen(s1);
-	count2 = strlen(s2);
-
-	str = malloc(sizeof(char) * (count1 + count2 + 1));
+	char *dup;
 
 	if (str == NULL)
-		retutn ('\0');
-	while (s[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s[j] != '\0')
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
-	return (str);
+		return ('\0');
+	dup = strdup(str);
+
+	if (dup == NULL)
+		return ('\0');
+
+	return (dup);
 }
