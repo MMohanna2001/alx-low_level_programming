d96239c2cf666ec01c6e59209e2e1336f8fee8f2
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-11 12:22:56 -0700

    11th

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || c == '\0')
 		return ('\0');
 	
-	arr = malloc(size(char) * size);
+	arr = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
