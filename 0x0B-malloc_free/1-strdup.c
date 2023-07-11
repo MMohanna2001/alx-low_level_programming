2a752aeb8200bcafe35c042fb51f57a3804af07f
MMohanna2001 <mohamedmohanna504@gmail.com>
2023-07-11 12:58:22 -0700

    11th

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Code
+ * @str: arg1
+ * Return: re
+ *
+ */
+
+char *_strdup(char *str);
+{
+
+	char *dup;
+
+	if (str == NULL)
+		return ('\0');
+	
+	dup = strdup(str);
+
+	if (dup == NULL)
+		return ('\0');
+
+	return ('\0');
+}
